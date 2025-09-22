2f5856c66fdcc76e41f36442aa5c7bfbfc736250
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-09-21 20:42:13 -0700

    Improve handling of multiple faces

--- AssetRipper.Conversions.Crunch/Crunch.cs
+++ AssetRipper.Conversions.Crunch/Crunch.cs
@@ -6,6 +6,16 @@ namespace AssetRipper.Conversions.Crunch;
 
 public static partial class Crunch
 {
+	/// <summary>
+	/// <see href="https://github.com/Unity-Technologies/crunch/blob/ea9b8d8c00c8329791256adafa8cf11e4e7942a2/inc/crnlib.h#L91"/>
+	/// </summary>
+	public const int MaxFaces = 6;
+
+	/// <summary>
+	/// <see href="https://github.com/Unity-Technologies/crunch/blob/ea9b8d8c00c8329791256adafa8cf11e4e7942a2/inc/crnlib.h#L92"/>
+	/// </summary>
+	public const int MaxLevels = 16;
+
 	public static bool TryDecompress(ReadOnlySpan<byte> input, [NotNullWhen(true)] out byte[]? output)
 	{
 		return TryDecompress(input, 0, out output);
@@ -23,6 +33,7 @@ public static partial class Crunch
 		fixed (byte* pInput = input)
 		{
 			crnd_crn_texture_info textureInfo = default;
+			textureInfo.field_0 = 36; // size of crnd_crn_texture_info
 			if (!crnd_get_texture_info(pInput, inputLength, &textureInfo))
 			{
 				return False(out output);
@@ -34,16 +45,33 @@ public static partial class Crunch
 				return False(out output);
 			}
 
+			// m_struct_size
 			int width = int.Max(1, textureInfo.field_1 >> levelIndex);
 			int height = int.Max(1, textureInfo.field_2 >> levelIndex);
-			int blocksX = int.Max(1, (width + 3) >> 2);
-			int blocksY = int.Max(1, (height + 3) >> 2);
+			// m_levels
+			int faceCount = textureInfo.field_4;
+			int bytesPerBlock = textureInfo.field_5;
+			// m_userdata0
+			// m_userdata1
+			// m_format
+
+			int blocksX = (width + 3) >> 2;
+			int blocksY = (height + 3) >> 2;
 			int rowPitch = blocksX * crnd_get_bytes_per_dxt_block(textureInfo.field_8);
-			int totalFaceSize = rowPitch * blocksY;
+			int faceSize = rowPitch * blocksY;
+			int totalFaceSize = faceCount * faceSize;
+
 			byte[] result = new byte[totalFaceSize];
+
 			fixed (byte* pResult = result)
 			{
-				if (!crnd_unpack_level(context, &pResult, totalFaceSize, rowPitch, levelIndex))
+				byte** pResultArray = stackalloc byte*[MaxFaces] { null, null, null, null, null, null };
+				for (int i = 0; i < faceCount; i++)
+				{
+					pResultArray[i] = pResult + i * faceSize;
+				}
+
+				if (!crnd_unpack_level(context, pResultArray, faceSize, rowPitch, levelIndex))
 				{
 					crnd_unpack_end(context);
 					return False(out output);
